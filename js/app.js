ea96af939cfb3613a72a184327d1acab3b1d3d68
Brayant Gualdron <zbgualdron@gmail.com>
2023-04-27 11:21:56 -0500

    Change project root for service worker

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+const url = window.location.href;
+let swLocation = "/pwa-twittor/sw.js";
+
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/sw.js");
+  if (url.includes("localhost")) {
+    swLocation = "/sw.js";
+  }
+
+  navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
